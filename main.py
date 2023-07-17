e71186c0dc6c0a33d22bb1cf19ccb48b57d719e8
MichelVillot <107226318+MichelVillot@users.noreply.github.com>
2023-07-17 15:41:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import db_dtypes
 from PIL import Image
 import time
+import streamlit_extras
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.metric_cards import style_metric_cards
 from streamlit_option_menu import option_menu
